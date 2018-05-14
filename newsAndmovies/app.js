8b35bc36095abc39923741aa6c7a5e6c975fe52d
fenerchen <1634465137@qq.com>
2018-05-14 11:50:22 +0800

    获取用户信息

--- newsAndmovies/app.js
+++ newsAndmovies/app.js
@@ -6,10 +6,31 @@ App({
 // var y=wx.getStorageSync()
 // console.log(x)
 // console.log(y)
+     wx.getSetting({
+       success:res=>{
+        //  console.log(res)
+         if (res.authSetting['scope.userInfo']){
+           wx.getUserInfo({
+
+             success:res=>{
+              //  console.log(res)
+               this.globalData.userInfo=res.userInfo
+               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+               // 所以此处加入 callback 以防止这种情况
+               if (this.userInfoReadyCallback) {
+                 this.userInfoReadyCallback(res)
+               }
+             }
+           })
+         }
+       }
+     })
   },
   globalData: {
     doubanBase:"https://douban.uieee.com",
     // movieList: []
-    movieList:[1,2,3]
-  }
+    movieList:[1,2,3],
+    userInfo: null
+  },
+  
 })
